f3cef0743c16423defc90488790627e4fa0a99b2
rishabhDev2700 <rishavkumar2700@gmail.com>
2021-11-21 07:16:57 +0530

    added department and pay_grade models

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
+
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=20)
+    department_description = models.CharField(max_length=50)
+
+
+class PayGrade(models.Model):
+    base_pay = models.IntegerField()
+    house_rent_allowance = models.IntegerField()
+    dearness_allowance = models.IntegerField()
+    bonus = models.FloatField()
 
 
 class User(AbstractUser):
@@ -16,10 +27,12 @@ class User(AbstractUser):
         ('HR', 'HR')
     ]
     mobile = models.CharField(max_length=10)
-    age = models.IntegerField(max_length=70)
+    age = models.IntegerField(null=True)
     role = models.CharField(choices=roles, max_length=10)
     is_admin = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    pay_grade = models.ForeignKey(PayGrade, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.username
